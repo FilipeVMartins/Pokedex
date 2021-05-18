331639c41534176b0cd9ccbc4dda0a3976bc5a1b
FilipeVMartins <filipe_2021@hotmail.com>
2021-05-17 22:42:18 -0300

    finished basic layout, changed search to on click

--- src/App.js
+++ src/App.js
@@ -12,7 +12,13 @@ export default class App extends React.Component {
     pRequestedList: [],
 
     selectedTypes: [],
-    inputedText: ''
+    inputedText: '',
+
+    searchingState: false,
+
+    validationDisplay: 'none',
+
+    displayTypeMenu: false
   }
 
   async componentDidMount(){
@@ -37,7 +43,7 @@ export default class App extends React.Component {
       // set pokemon list to be searched
       this.setState({pNames:response[0].results})
 
-      // none of this would work becaouse bot all pokemons are indexed in its api type pokemon list
+      // this would've work BUT not all pokemons are indexed in its api type pokemon list, this method would be much more fast on the searchs, it would create a index with 'name', 'url' AND the given 'pokemon types' within each registry, allowing faster searchs with the 'type filtering' available before the search requests.
       // // each object in this array will recieve one more element containing the types of its pokemon after the next request
       // let pNamesAndTypes = response[0].results.map((pokemon) => {
       //   return {'name':pokemon.name, 'url':pokemon.url, 'types':[] }
@@ -93,17 +99,24 @@ export default class App extends React.Component {
 
     // sets the searched text
     this.setState({inputedText:evt.target.value})
+  };
+
+  submitSearchHandler = (evt) => {
+    evt.preventDefault();
+
+    this.setState({searchingState: true})
 
     // starts pokemon filtering search and requests after a minimum input of characters
-    if (evt.target.value.length > 1 ){
-      
+    if (this.state.inputedText.length > 1 ) {
+      this.setState({validationDisplay: 'none'})
+          
       // searched list
       let pNamesSearched = [];
       // loop pokemons list
       Object.entries(this.state.pNames).forEach(([key, pokemon]) => {
 
         // if inputed value matches pokemons name
-        if (pokemon.name.includes(evt.target.value)){
+        if (pokemon.name.includes(this.state.inputedText.toLowerCase())){
 
           // push pokemon given registry to searched list
           pNamesSearched.push(pokemon);
@@ -116,8 +129,9 @@ export default class App extends React.Component {
       
 
     } else {
-      // if less than defined before, clear results
-      this.setState({pNamesSearched: []}, this.getRequestedList);
+      // needs at least one character
+      this.setState({validationDisplay: 'block'});
+      this.setState({searchingState: false});
     }
   };
 
@@ -140,8 +154,8 @@ export default class App extends React.Component {
     const P = new Pokedex.Pokedex()
     // make the request with the created resourceStringList
     P.resource(resourceStringList).then( response => {
-      // add the requested results to 'pRequestedList'
-      this.setState({pRequestedList: response});
+      // add the requested results to 'pRequestedList', and after, apply filter
+      this.setState({pRequestedList: response}, this.setState({searchingState: false}) );
     });
   }
 
@@ -157,6 +171,39 @@ export default class App extends React.Component {
     return false;
   };
 
+  // removes non-selected pokemon types from 'pRequestedList' after it's been set
+  pTypesFilter = (pokemon) => {
+    
+
+    // if none type selected, then return all
+    if ( this.state.selectedTypes.length == 0 ){
+      return true
+    } else {
+      // else, apply the filter
+      let isPokemonOfSelectedType;
+      //let pRequestedListFiltered = this.state.pRequestedList.filter( (pokemon) => {
+
+        // search select types
+        pokemon.types.forEach( (type) => {
+          if ( this.state.selectedTypes.includes(type.type.name)  ){
+            // if found a selected type in a pokemon return yes
+            isPokemonOfSelectedType = true;
+          } else {
+            // else return false
+            isPokemonOfSelectedType = false;
+          };
+        });
+        // return filter results
+        if (isPokemonOfSelectedType){
+          return true
+        } else {
+          return false
+        };
+      //});
+      //this.setState({pRequestedList:pRequestedListFiltered})
+    };
+  };
+
   // // gets all data of one pokemon from local 'pRequestedList'
   // getOnePokemonData = (pokemonName) => {
   //   //console.log(pokemonName)
@@ -168,6 +215,43 @@ export default class App extends React.Component {
   //   return false
   // };
 
+  clearResults = () => {
+    // if less than defined before, clear results
+    this.setState({pRequestedList: []});
+    this.setState({selectedTypes: []});
+    this.setState({inputedText: ''});
+  }
+
+  // back to top function
+  backToTop = (evt) => {
+    evt.preventDefault();
+    document.querySelector('.pokemon-results').scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+    
+    
+  }
+
+  functionDisplayTypeMenu = () => {
+
+    if (this.state.displayTypeMenu) {
+      this.setState({displayTypeMenu: false})
+      return
+    } else {
+      this.setState({displayTypeMenu: true})
+    }
+  }
+
+  //capitalize only the first letter of the string. 
+  capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  //capitalize all words of a string. 
+  capitalizeWords(string) {
+    return string.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+  };
+
 
 
   render() {
@@ -176,41 +260,63 @@ export default class App extends React.Component {
       <div className="app">
 
         <header className="app-header">
-          <h1 onClick={() => console.log(this.state.pRequestedList)}>Pokédex</h1>
-          <form>
-            <input type="text" value={this.state.inputedText} onChange={this.onChangeInputHandler}></input>
+          <h1 className="header-title">Pokédex</h1>
+
+          <form disabled={this.state.searchingState} onSubmit={this.submitSearchHandler} >
+            <small style={{display: this.state.validationDisplay}}>Search needs to be at least two characters long</small>
+            <input className="input-pokemon-name" disabled={this.state.searchingState} type="text" value={this.state.inputedText} onChange={this.onChangeInputHandler}></input>
             <div className="select-pokemon-types">
-              <h4>Selecione os tipos</h4>
-              <div className="pokemon-types">
+
+              <h4 onClick={this.functionDisplayTypeMenu} className={`${this.state.displayTypeMenu ? 'select-type-clicked' : ''}`} >Select Pokémon Types</h4>
+
+              <div className={`pokemon-types ${this.state.displayTypeMenu ? 'openned' : ''}`}            > 
                 {this.state.pTypes.map((type) => {
                   return (
-                    <div key={`type-${type}`} className={`select-type ${type} ${this.state.selectedTypes.indexOf(type) !== -1 ? 'selected' : ''}`} onClick={() => this.changeSelectedTypes(type)} >{type}</div>
+                    <div key={`type-${type}`} className={`select-type ${type} ${this.state.selectedTypes.indexOf(type) !== -1 ? 'selected' : ''}`} onClick={() => this.changeSelectedTypes(type)} >{this.capitalizeFirstLetter(type)}</div>
                   )
                 })}
               </div>
+
+            </div>
+            <div className="buttons-wrapper" >
+              <button type="reset" disabled={this.state.searchingState} onClick={ () => this.clearResults()} >Clear Results</button>
+              <button type="submit" disabled={this.state.searchingState} >Search</button>
+              <button disabled={this.state.searchingState} onClick={ (evt) => this.backToTop(evt)} >Back to Top</button>
             </div>
           </form>
         </header>
 
-
+        
         <div className="app-content">
           <div className="pokemon-results">
 
             { this.state.pRequestedList.length != 0 ?
               this.state.pRequestedList.map( (pokemonData, index) => {
                 // because it starts rendering 6 rows by default
-                if (true){
+                if (this.pTypesFilter(pokemonData)){
 
 
                   return (
                     <div className="pokemon-box" key={'pokemon-box-' + index}>
-                      {pokemonData.name}
-                      {pokemonData.types[0].type.name}
+                      <figure>
+                        <img src={pokemonData.sprites.front_default} alt="Trulli" />
+                        <figcaption className="pokemon-name">{this.capitalizeWords(pokemonData.name.replace(/\-/g, " "))}</figcaption>
+                      </figure>
+
+                      <div className="id-pokemon-types">
+                          { pokemonData.types.map ( (type, index) => {
+                            return (
+                              <div className="pokemon-type" key={'pokemon-type'+index}>{type.type.name}</div>
+                            );
+                          })}
+                      </div>
+
+                      <div className="pokemon-id">#{pokemonData.id}</div>
                     </div>
-                  )
+                  );
                 };
               })
-            : ''}
+            : <strong className="result-not-found">No Pokémon Found!</strong>}
 
 
 
@@ -222,7 +328,7 @@ export default class App extends React.Component {
       
         <footer className="app-footer">
             <div>
-              <p>asdasd</p>
+              <p></p>
             </div>
         </footer>
     </div>
